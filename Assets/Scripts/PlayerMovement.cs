b1bf91a3bc5f7b4726e2b4bfcf22be475850e18b
KevYRS <43802321+KevYRS@users.noreply.github.com>
2024-12-14 00:53:31 -0600

    screens started title done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
     // Start is called before the first frame update
 
     private Vector2 moveDirection;
@@ -20,51 +19,40 @@ public class PlayerMovement : MonoBehaviour
     AudioSource sonidosJuego;
     public AudioClip gema;
 
-    void Start()
-    {
+    public GameObject Win;
+    public GameObject Lose;
+
+    void Start(){
         Contador = 0;
         sonidosJuego = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Update(){
         txtContador.text = "" + Contador; //score
         Movement();
         Animation();
     }
 
-
-    private void Movement()
-
-    {
+    private void Movement(){
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-
         moveDirection = new Vector2(moveX, moveY).normalized;
-
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 
-
-    private void Animation()
-
-    {
+    private void Animation(){
         anim.SetFloat("moveX", moveDirection.x);
         anim.SetFloat("moveY", moveDirection.y);
     }
 
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("gema"))
-        {
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("gema")){
             //anillo.gameObject.SetActive(false);
 
             Destroy(other.gameObject);
             Contador += 1;
-             sonidosJuego.PlayOneShot(gema);
+            sonidosJuego.PlayOneShot(gema);
         }
         
     }
