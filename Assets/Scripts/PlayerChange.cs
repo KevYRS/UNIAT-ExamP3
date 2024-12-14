cf005a216f550eec6b10521e34e1897e7cbe8ee8
JimeTg98 <jimenatg98@gmail.com>
2024-12-13 20:04:10 -0600

    levels

--- Assets/Scripts/PlayerChange.cs
+++ Assets/Scripts/PlayerChange.cs
@@ -46,6 +46,18 @@ public class PlayerChange : MonoBehaviour
 
         }
 
+
+        if (collision.gameObject.tag == "Area1.2")
+        {
+            Vector3 camPosition = new Vector3(0f, 0f, -10);
+            cam.transform.position = camPosition;
+
+
+            Vector3 playerPosition = new Vector3(0.49f, -2.92f, 0);
+            player.transform.position = playerPosition;
+
+        }
+
         if (collision.gameObject.tag == "Area4")
         {
             Vector3 camPosition = new Vector3(42.2f, 32.49f, -10);
@@ -57,8 +69,19 @@ public class PlayerChange : MonoBehaviour
 
         }
 
+        if (collision.gameObject.tag == "Area4.1")
+        {
+            Vector3 camPosition = new Vector3(42.2f, 32.49f, -10);
+            cam.transform.position = camPosition;
 
-        
+
+            Vector3 playerPosition = new Vector3(45.7f, 34.4f, 0);
+            player.transform.position = playerPosition;
+
+        }
+
+
+
         if (collision.gameObject.tag == "Area2")
         {
             Vector3 camPosition = new Vector3(22.54f, 0f, -10);
@@ -78,6 +101,31 @@ public class PlayerChange : MonoBehaviour
 
 
             Vector3 playerPosition = new Vector3(20.15f, 0.6f, 0);
+            player.transform.position = playerPosition;
+
+        }
+
+
+        if (collision.gameObject.tag == "Area5")
+        {
+            Vector3 camPosition = new Vector3(0.95f, -26.13f, -10);
+            cam.transform.position = camPosition;
+
+
+            Vector3 playerPosition = new Vector3(7.2f, -23.4f, 0);
+            player.transform.position = playerPosition;
+
+        }
+
+
+
+        if (collision.gameObject.tag == "Area6")
+        {
+            Vector3 camPosition = new Vector3(43.65f, 57.17f, -10);
+            cam.transform.position = camPosition;
+
+
+            Vector3 playerPosition = new Vector3(43.9f, 55.2f, 0);
             player.transform.position = playerPosition;
 
         }
